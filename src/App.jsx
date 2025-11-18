351cbb9fd8d134fab153ab53238076cb32ad6461
Isira Adithya <isira.weerasinghe@arimaclanka.com>
2025-11-18 23:15:38 +0530

    remode adminpges from this

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CustomerLayout from './components/customer/CustomerLayout';
-import AdminLayout from './components/admin/AdminLayout';
 import Home from './pages/customer/Home';
 import Gallery from './pages/customer/Gallery';
 import Registration from './pages/customer/Registration';
 import Booking from './pages/customer/Booking';
-import AdminPinScreen from './pages/admin/AdminPinScreen';
-import AdminDashboard from './pages/admin/AdminDashboard';
-import AdminBookings from './pages/admin/AdminBookings';
-import AdminRegistrations from './pages/admin/AdminRegistrations';
-import AdminSettings from './pages/admin/AdminSettings';
 
 function App() {
   return (
@@ -21,15 +15,6 @@ function App() {
           <Route path="gallery" element={<Gallery />} />
           <Route path="register" element={<Registration />} />
           <Route path="booking" element={<Booking />} />
-        </Route>
-
-        {/* Admin Routes */}
-        <Route path="/admin" element={<AdminPinScreen />} />
-        <Route path="/admin/portal" element={<AdminLayout />}>
-          <Route index element={<AdminDashboard />} />
-          <Route path="bookings" element={<AdminBookings />} />
-          <Route path="registrations" element={<AdminRegistrations />} />
-          <Route path="settings" element={<AdminSettings />} />
         </Route>
       </Routes>
     </BrowserRouter>
